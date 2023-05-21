b5e0642ae90e5d3f06cffcecfc3f73dea5fcf00b
Peter Eddy <peter.eddy@gmail.com>
2023-05-21 12:53:46 -0400

    Initial Commit

---
+++ MessageThing/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MessageThing
+//
+//  Created by Peter Eddy on 5/21/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
